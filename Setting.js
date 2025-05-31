d9cb4e25ceee2d58dff81e8b8b379418bc1cfee6
Maxwel _Odhiambo <himaxwell36@gmail.com>
2025-05-31 13:54:04 +0300

    Create Setting.js

---
+++ Setting.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'Undefined',
+  author: '‎',
+  botName: "undefined",
+  botOwner: 'Maxinto', // Your name
+  ownerNumber: '+254739339209', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.0",
+};
+
+module.exports = settings;
